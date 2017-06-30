6748b0cb096ce097a36cc83d8bf170faea6ed02d
Anthony Tjuatja <anthonytjuatja_14@yahoo.com>
2017-06-30 23:42:51 +0800

    Implemented fully working web-crawling.

--- src/main/java/tm/parser/Crawler.java
+++ src/main/java/tm/parser/Crawler.java
@@ -1,12 +1,12 @@
 package tm.parser;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,7 +32,7 @@ public class Crawler {
 
 
         while (listOfPatentsURL.size() < 20) {
-            System.out.println(driver.getCurrentUrl());
+            //System.out.println(driver.getTitle());
             long end = System.currentTimeMillis() + 20000;
             while (System.currentTimeMillis() < end) {
                 ArrayList<WebElement> resultsDiv = (ArrayList<WebElement>) driver.findElements(By.tagName("search-result-item"));
@@ -47,12 +47,58 @@ public class Crawler {
                 System.out.println(result.getAttribute("open-result"));
             }
 
-            Thread.sleep(1000);
-            System.out.println();
-            WebElement nextButton = new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//state-modifier[@page='next']"))));
+            WebElement nextButton = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//paper-icon-button[@icon='chevron-right']")));
             nextButton.click();
+
+            //Codes below Attempt to get the page header number to identify that the next page has finished loading
+            // List<WebElement> pageCount =  driver.findElements(By.xpath("//div[@class='count style-scope search-results']//span[@class='style-scope search-results']"));
+            // System.out.println(pageCount.getText());
+            // for (WebElement pageHeaderElement : pageCount) {
+            //System.out.println(pageHeaderElement.getText());
+            // }
+
+
+            //Current workaround to ensure the AJAX call to ensure correctness of fetching new patents is to put timeout of 5 seconds
+            Thread.sleep(5000);
+
+            //The code below does not provide positive for this context since nextButton.click() is already a blocking call,
+            // but may when the features are extended
+            while (!waitForJStoLoad(driver)) {
+            }
         }
-        driver.quit();
+         driver.quit();
+        startHTMLParser(listOfPatentsURL);
+    }
+
+    public static void startHTMLParser(ArrayList<String> urls) {
+        HTMLParser.execute( urls.toArray(new String[urls.size()]));
+    }
+
+    public static boolean waitForJStoLoad(WebDriver driver) {
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        // wait for jQuery to load
+        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                try {
+                    return ((Long) ((JavascriptExecutor) driver).executeScript("return jQuery.active") == 0);
+                } catch (Exception e) {
+                    return true;
+                }
+            }
+        };
+
+        // wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState")
+                        .toString().equals("complete");
+            }
+        };
+
+        return wait.until(jQueryLoad) && wait.until(jsLoad);
     }
 }
 
--- src/main/java/tm/parser/HTMLParser.java
+++ src/main/java/tm/parser/HTMLParser.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  * Created by antho on 6/7/2017.
@@ -17,40 +18,19 @@ import java.io.PrintWriter;
 public class HTMLParser {
 
     public static final String MASTER_URL = "https://patents.google.com/?q=Google&assignee=Google+Inc.";
-    public static String[] urls = new String[]{
-            "https://patents.google.com/patent/US7933897B2/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US20120066296A1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US20080301643A1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US8291492B2/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US7634463B1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US7685144B1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US8185830B2/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US7353114B1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US7978207B1/en?q=Google&assignee=Google+Inc.",
-            "https://patents.google.com/patent/US20080183593A1/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US7933929B1/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8010407B1/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8504437B1/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8060582B2/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8201081B2/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US7031961B2/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US7664734B2/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8291341B2/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US20080172373A1/en?q=Google&assignee=Google+Inc.&page=1",
-            "https://patents.google.com/patent/US8793509B1/en?q=google&assignee=Google+Inc.&page=2"
-    };
+    public static String[] urls;
 
     public HTMLParser() throws IOException {
 
     }
 
-    public static void main(String[] args) {
 
-
+    public static void execute(String[] patentsURL) {
+        urls = patentsURL;
         try {
             Document mainDoc = Jsoup.connect(MASTER_URL).get();
             for (int i = 0; i < 20; i++) {
-                System.out.println("Parsing document number : " + i);
+                System.out.println("Parsing document from : " + urls[i]);
                 Document doc = Jsoup.connect(urls[i]).get();
                 Element searchApp = doc.select("search-app").first();
                 String textdesc = searchApp.select("section[itemprop=description]").first().select("[mxw-id]").first().text();
@@ -61,5 +41,6 @@ public class HTMLParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
